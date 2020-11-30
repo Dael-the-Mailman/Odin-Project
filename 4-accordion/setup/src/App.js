cabc3683046280798da6bf2a98ed8a5f4978461a
Dael-the-Mailman <kalebugalde@gmail.com>
2020-11-30 11:19:15 -0800

    Finished 4-Accordian by myself :)

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
+function App() {  
+  return (
+  <main>
+    <div className='container'>
+      <h3>questions and answers about login</h3>
+      <div>
+        {data.map((item) => {
+          return <SingleQuestion id={item.id} 
+                                 title={item.title} 
+                                 info={item.info}
+                                 />
+        })}
+      </div>
+    </div>
+  </main>);
 }
 
 export default App;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({id, title, info}) => {
+  const [show, setShow] = useState(false)
+  const showInfo = () =>{
+    setShow(!show);
+  }
+
+  return (
+    <div className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={showInfo}>
+          {(show) ? <AiOutlinePlus/> : <AiOutlineMinus/>}
+        </button>
+      </header>
+      {(show) ? <p>{info}</p> : <></>}
+    </div>
+  );
 };
 
 export default Question;
